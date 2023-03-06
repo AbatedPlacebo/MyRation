28f222eca356d612947324d59e0c1f7622580eee
Norman5k <48478343+Norman5k@users.noreply.github.com>
2023-03-06 16:27:50 +0700

    fragment_home.xml + HomeFragment.java с примером. bottom_sheet_layout.xml можно скролить.

--- app/src/main/java/com/example/test/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/test/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.test.ui.home;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,11 +15,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.test.MainActivity;
+import com.example.test.R;
 import com.example.test.databinding.FragmentHomeBinding;
+import com.example.test.ui.dashboard.DashboardFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private ListView listView;
+    private TextView textView_stats;
+    private TextView textView_stage;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +36,18 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        listView = root.findViewById(R.id.listview_products);
 
-        //final TextView textView = binding.textHome;
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        textView_stats = root.findViewById(R.id.text_stats);
+        textView_stage = root.findViewById(R.id.text_stage);
+        textView_stage.setText("1 из 4");
+        textView_stats.setText("Жиры: 20\nБелки: 15\nУглеводы: 100");
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<3;i++)
+            list.add("Item "+i);
+        CustomAdapter listAdapter = new CustomAdapter(list);
+        listView.setAdapter(listAdapter);
+
         return root;
     }
 
@@ -35,5 +55,35 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public class CustomAdapter extends BaseAdapter {
+        List<String> items;
+
+        public CustomAdapter(List<String> items) {
+            super();
+            this.items = items;
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return items.get(i).hashCode();
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(getContext());
+            textView.setText(items.get(i));
+            return textView;
+        }
     }
 }
